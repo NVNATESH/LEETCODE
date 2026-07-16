7e8a19cbb06ee9abc095665d03201d67b0c1bebe
NVNATESH <nvnatesh.1@gmail.com>
2026-07-16 12:06:25 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.12%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1 && k==1)return 0;
+        else if(n==2)
+        {
+        if(k==1)return 0;
+        if(k==2)return 1;
+        }
+        int p=kthGrammar(n-1,(k+1)/2);
+        if(k%2==1)return p;
+        return 1-p;
+    }
+};
