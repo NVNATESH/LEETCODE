2697265a1cb4e5c436f43be05deed20a1139771a
NVNATESH <nvnatesh.1@gmail.com>
2026-08-13 23:06:13 +0530

    Time: 0 ms (100%), Space: 14.4 MB (88.06%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& r, vector<bool>& v,int src){
+        v[src] = true;
+        for(int a:r[src]){
+            if(!v[a]){
+                dfs(r,v,a);
+            }
+        }
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        vector<int> v(n);
-        stack<int> q;
-        for(int i=0;i<rooms[0].size();i++){
-            q.push(rooms[0][i]);
-        }
-        while(!q.empty()){
-            int a = q.top();
-            q.pop();
-            if(v[a]==1){
-                continue;
-            }
-            for(int i=0;i<rooms[a].size();i++){
-                q.push(rooms[a][i]);
-            }
-            v[a] = 1;
-        }
-        for(int i=1;i<v.size();i++){
-            if(v[i]==0) return false;
+        vector<bool> v(n,false);
+        dfs(rooms,v,0);
+        for(int i=0;i<v.size();i++){
+            if(v[i]==false) return false;
         }
         return true;
     }
