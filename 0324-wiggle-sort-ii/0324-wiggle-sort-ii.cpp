85a0a7a2fd9210c1b6a9fa92ab9fd9628280f4b8
NVNATESH <nvnatesh.1@gmail.com>
2026-09-08 21:25:10 +0530

    Time: 11 ms (5.57%), Space: 22.4 MB (13.95%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        priority_queue<int> pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+        // int j = nums.size()%2==0 ? nums.size()/2 : nums.size()/2+1;
+        for(int j=1;j<nums.size();j+=2){
+            nums[j] = pq.top();
+            pq.pop();
+        }
+        for(int j = 0;j<nums.size();j+=2){
+            nums[j] = pq.top();
+            pq.pop();
+        }
+    }
+};
